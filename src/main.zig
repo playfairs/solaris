44e7c1822c3a247d9c322b6ef1036f41dd784ef3
playfairs <root@playfairs.cc>
2026-05-04 15:32:15 -0500

    ok

--- src/main.zig
+++ src/main.zig
@@ -3,10 +3,10 @@ const std = @import("std");
 const App = @import("platform/macos/App.zig");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    
-    const allocator = gpa.allocator();
+    var arena: std.heap.ArenaAllocator = .init(std.heap.page_allocator);
+    defer _ = arena.deinit();
+
+    const allocator = arena.allocator();
 
     var app = try App.init(allocator);
     defer app.deinit();
